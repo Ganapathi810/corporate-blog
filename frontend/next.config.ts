1a80898ed154b417f3920ef202b4f7978e2e2e8f
O V V S Ganapathi <othojuganapathi123@gmail.com>
2026-06-19 13:08:31 +0530

    added cloudinary domain to next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,8 +6,12 @@ const nextConfig: NextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
+                hostname: '*.googleusercontent.com',
             },
+            {
+              protocol: 'https',
+              hostname: 'res.cloudinary.com'
+            }
         ],
     },
 };
