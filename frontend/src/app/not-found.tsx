fd1d2ded60a489ca6e932044d28a57f641469224
O V V S Ganapathi <othojuganapathi123@gmail.com>
2026-06-28 18:25:09 +0530

    added not-found pages to / and /blog

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NotFoundPage() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold text-blue-400">404</h1>
+                <p className="text-xl text-blue-300">Page not found</p>
+                <Link href="/" className="text-blue-500 hover:underline mt-4 block">Back to home</Link>
+            </div>
+        </div>
+    )
+}
